865a19009c6cd93a6a899ee1e268c1c73a803647
mathewz007 <mathew.matthew@gmail.com>
2016-03-01 19:24:56 -0500

    EL

---
+++ Guessing_Game_EL/src/model/Message.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String msg;
+
+	public Message() {
+		msg="";
+	}
+
+	public Message(String msg) {
+		this.msg = msg;
+	}
+
+	/**
+	 * @return the msg
+	 */
+	public String getMsg() {
+		return msg;
+	}
+
+	/**
+	 * @param msg the msg to set
+	 */
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
